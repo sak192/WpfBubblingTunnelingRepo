e5e7e6ab3a79dab01331b6d8174e7d741e7e6f99
sak192 <67677434+sak192@users.noreply.github.com>
2025-09-04 09:30:01 +0530

    Add files via upload

---
+++ ModelViewMV.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace WpfApp_BubblingAndTunnelingEvents.ViewModel
+{
+    public class ModelViewMV
+    {
+        public string result { get; set; }
+
+        public String Result
+        {
+            get { return result; }
+            set { result = value; }
+        }
+
+        public ModelViewMV()
+        {
+            Result = "abc";
+        }
+
+    }
+}
